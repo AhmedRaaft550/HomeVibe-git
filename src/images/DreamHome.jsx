d9ceea8a8184a027eb920f7248c6064552668d0f
Ahmedraaft232 <ahmed.raaft250@gmail.com>
2025-02-14 18:33:42 +0400

    imgUpdate

--- src/images/DreamHome.jsx
+++ src/images/DreamHome.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import dreamHomeImg from './dreamhome.jpg'
 
 
 function DreamHome() {
   return (
 <div className="dream-home">
 
-<img src="/src/images/dreamhome.jpg" alt="" />
+<img src= {dreamHomeImg} alt="" />
 <p className='dream-text'>Tailor Your Comfort <br/> Customize Your Perfect Piece, <br/> Just the Way You Want It! </p>
 </div>
   )
